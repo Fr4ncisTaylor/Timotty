5efbb833b20e228f638a7fb0516b58a5c628104e
Francis Taylor <francistrapp2000@gmail.com>
2017-10-19 15:42:28 -0300

    Create sendReply.py

---
+++ exemplos/sendReply.py
@@ -0,0 +1,7 @@
+```bash
+from metodos import sendReply ## Importa a função
+
+def exemplo(msg):
+    sendReply(msg, 'Hello World') ## envia a mensagem com reply
+    
+""" A função sendReply é nativa do Timotty :) """
