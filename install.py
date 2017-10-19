48caa35f2a33225e595d07fe6bc91be6986196e3
Francis Taylor <francistrapp2000@gmail.com>
2017-10-19 10:25:58 -0400

    install for python3

--- install.py
+++ install.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 import os,sys,time
 
-if raw_input(u'Deseja iniciar instalação do Timotty? [Y/N]\n\n>>> ').lower() == 'y':
+if input(u'Deseja iniciar instalação do Timotty? [Y/N]\n\n>>> ').lower() == 'y':
     print(u'Iniciando...')
 
     try:
         import requests
     except:
-        print(u'instalando Requests')
-        os.system('pip install requests')
+        print(u'Instalando Requests')
+        os.system('pip3 install requests')
     try:
         import redis
     except:
-        print(u'instalando Redis-Server')
-        os.system('pip install redis')
+        print(u'Instalando Redis-Server')
+        os.system('pip3 install redis')
 
     os.system('clear')
     print(u'Instalação concluída!')
     time.sleep(2)
     os.system('clear')
-    print('abra o arquivo config.py e edite-o conforme os exemplos.')
-    time.sleep(5)
-
-    os.system('clear')
-    sys.exit()
+    if input('Deseja iniciar Timotty? [Y/N]\n\n> '):
+        os.system('python3 bot.py')
+    else:
+        os.system('clear')
+        sys.exit()
 
 else:
     sys.exit()
